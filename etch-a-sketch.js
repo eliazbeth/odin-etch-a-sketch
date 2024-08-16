375a22cde5818dd8a302a79db28de30b814b0cfc
Liz <elizabethhernandez.fl.@gmail.com>
2024-08-16 12:37:58 -0400

    Add button to change grid size (length of its sides)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,35 @@
 const grid = document.querySelector("#grid");
 
+var size = 4;
+const sizeButton = document.querySelector("#sizeButton");
+sizeButton.addEventListener("click", changeSize);
+
 function changeColor(div)
 {
     div.style.backgroundColor = "red";
+}
+
+function changeSize()
+{
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => { grid.removeChild(square); }); 
+    size = prompt("Enter a size for the grid: ");    
+    squareSize = 200/size;    // Square height/width are total pixels(200) divided by number of columns/rows
+    console.log(size);
+    console.log(squareSize);
+    for (i = 0; i < size; i++)
+    {
+        for(j = 0; j < size; j++)
+        {
+            console.log("in loop");
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.style.width = squareSize + "px";
+            square.style.height = squareSize + "px";
+            square.addEventListener("mouseover", () => changeColor(square)); 
+            grid.appendChild(square);
+        }
+    }
 }
 
 for (i = 0; i < 16; i++)
