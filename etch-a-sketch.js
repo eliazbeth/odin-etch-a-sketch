33ab510fe84eeb22cd6bf377661d35122ca49c46
Liz <elizabethhernandez.fl.@gmail.com>
2024-08-16 11:51:21 -0400

    Add event listener to squares so background changes to red upon being hovered over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,14 @@
 const grid = document.querySelector("#grid");
+
+function changeColor(div)
+{
+    div.style.backgroundColor = "red";
+}
+
 for (i = 0; i < 16; i++)
 {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.addEventListener("mouseover", () => changeColor(square)); 
     grid.appendChild(square);
 }
