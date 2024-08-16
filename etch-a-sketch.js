67c3be000bb22e5dbe746b11d6beb622e124b92d
Liz <elizabethhernandez.fl.@gmail.com>
2024-08-16 11:44:25 -0400

    Mkae 16x16 grid of div squares using flexbox

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,5 +2,6 @@ const grid = document.querySelector("#grid");
 for (i = 0; i < 16; i++)
 {
     const square = document.createElement("div");
+    square.setAttribute("class", "square");
     grid.appendChild(square);
 }
